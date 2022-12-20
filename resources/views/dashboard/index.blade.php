c117c55d62ecf7a45f3f730c19e50e2399d00904
rmeredith3 <ricky@techuso.com>
2022-12-19 23:45:29 -0700

    First commit, base for the project

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container text-center p-3">
+		<div class="row justify-content-center mt-4">
+			<div class="col-6 text-center">
+				<h1>Cashback Calculator</h1>
+			</div>
+		</div>
+		<div class="row justify-content-end">
+			<div class="col-4">
+				
+			</div>
+		</div>
+		<div class="row justify-content-around mt-5">
+			<div class="col-7">
+				<div class="card">
+					<div class="card-header">
+						<h2>Calculator</h2>
+					</div>
+					<div class="card-body">
+	
+					</div>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="card">
+					<div class="card-header">
+						<div class="row justify-content-between">
+							<div class="col-8">
+								<h2>My Cards</h2>
+							</div>
+							<div class="col-4">
+								<button class="btn btn-primary">
+									New Card
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="card-body">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
